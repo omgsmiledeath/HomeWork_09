a68a19582f10bc140e9e86b9556a27a583aa66e2
Петров Олег <araragikyn@hotmail.com>
2020-02-18 14:35:26 +0800

    пока не работает

--- HomeWork_09/Program.cs
+++ HomeWork_09/Program.cs
@@ -3,13 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
 
 namespace HomeWork_09
 {
     class Program
     {
-        static void Main(string[] args)
+        static TelegramBotClient botClient;
+        public static void Main(string[] args)
         {
+
+            string token = "1095469268:AAHx8NoO7r5iwcPsbeqGIQC70mTakIGkk9M";
+
+            var proxy = new WebProxy()
+            {
+                Address = new Uri($"http://207.154.231.216:3128"),
+                UseDefaultCredentials = false
+            };
+
+            var httpCliendHandler = new HttpClientHandler() { Proxy = proxy };
+            HttpClient hc = new HttpClient(httpCliendHandler);
+
+            botClient = new TelegramBotClient(token,hc);
+            // var me = botClient.GetMeAsync().Result;
+            // Console.WriteLine(me.Username);
+
+                botClient.OnMessage += HanldeMessage;
+                botClient.StartReceiving();
+
+            Console.ReadKey();
+        }
+
+        static void HanldeMessage(object sender, Telegram.Bot.Args.MessageEventArgs e )
+        {
+            //Console.WriteLine(e.Message.Text);
+            var chatId = e.Message.Chat.Id;
+            botClient.SendTextMessageAsync(chatId, "Дратути");
         }
     }
-}
+
+         
+    }
+
