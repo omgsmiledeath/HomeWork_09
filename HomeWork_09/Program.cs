1059ee34c95d4e559df00133ec34e4463ca39d9c
Петров Олег <araragikyn@hotmail.com>
2020-02-18 16:07:06 +0800

    1

--- HomeWork_09/Program.cs
+++ HomeWork_09/Program.cs
@@ -12,36 +12,37 @@ namespace HomeWork_09
 {
     class Program
     {
-        static TelegramBotClient botClient;
+        static ITelegramBotClient botClient;
         public static void Main(string[] args)
         {
 
-            string token = "1095469268:AAHx8NoO7r5iwcPsbeqGIQC70mTakIGkk9M";
+            string token = "1095469268:AAE7JJq1Xb0MrVQvcAKqbfsQHKJUSl2LgGQ";
 
             var proxy = new WebProxy()
             {
-                Address = new Uri($"http://207.154.231.216:3128"),
-                UseDefaultCredentials = false
+                Address = new Uri($"http://162.243.108.129:3128"),
+                UseDefaultCredentials = false,
             };
 
             var httpCliendHandler = new HttpClientHandler() { Proxy = proxy };
             HttpClient hc = new HttpClient(httpCliendHandler);
 
-            botClient = new TelegramBotClient(token,hc);
-            // var me = botClient.GetMeAsync().Result;
-            // Console.WriteLine(me.Username);
-
+            botClient = new TelegramBotClient(token, hc) ;
+            var me = botClient.GetMeAsync().Result;
+             Console.WriteLine(me.Username);
+                
                 botClient.OnMessage += HanldeMessage;
                 botClient.StartReceiving();
 
             Console.ReadKey();
         }
 
-        static void HanldeMessage(object sender, Telegram.Bot.Args.MessageEventArgs e )
+        static async void HanldeMessage(object sender, Telegram.Bot.Args.MessageEventArgs e )
         {
+            string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
             //Console.WriteLine(e.Message.Text);
             var chatId = e.Message.Chat.Id;
-            botClient.SendTextMessageAsync(chatId, "Дратути");
+           await botClient.SendTextMessageAsync(chatId, "Дратути");
         }
     }
 
