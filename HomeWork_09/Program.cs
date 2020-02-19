4483673e66a55b2456e6682502755f8a7767c515
Holiday <Holiday@DESKTOP-54GAQ21>
2020-02-19 22:54:53 +0800

    Сделал истоию сообщений

--- HomeWork_09/Program.cs
+++ HomeWork_09/Program.cs
@@ -7,42 +7,59 @@ using Telegram.Bot;
 using Newtonsoft.Json;
 using System.Net;
 using System.Net.Http;
-
+using System.IO;
 namespace HomeWork_09
 {
     class Program
     {
-        static ITelegramBotClient botClient;
+        static TelegramBotClient botClient;
+        static List<TelegramUser> users;
         public static void Main(string[] args)
         {
 
-            string token = "1095469268:AAE7JJq1Xb0MrVQvcAKqbfsQHKJUSl2LgGQ";
+            string token = "1095469268:AAFhHfoGHRsuFTnvua96UqQxbU4fp3Osegw";
+            string json1 = File.ReadAllText("users.txt");
+            
 
+            users = JsonConvert.DeserializeObject<List<TelegramUser>>(json1);
+            if (users == null) users =  new List<TelegramUser>();
             var proxy = new WebProxy()
             {
-                Address = new Uri($"http://162.243.108.129:3128"),
+                Address = new Uri($"http://163.172.146.119:8811"),
                 UseDefaultCredentials = false,
             };
 
             var httpCliendHandler = new HttpClientHandler() { Proxy = proxy };
             HttpClient hc = new HttpClient(httpCliendHandler);
 
-            botClient = new TelegramBotClient(token, hc) ;
-            var me = botClient.GetMeAsync().Result;
-             Console.WriteLine(me.Username);
-                
-                botClient.OnMessage += HanldeMessage;
-                botClient.StartReceiving();
+            botClient = new TelegramBotClient(token, hc);
+            var u = botClient.TestApiAsync().Result;
+            Console.WriteLine(u);
+            //var me = botClient.GetMeAsync().Result;
+            // Console.WriteLine(me.Username);
+
+            botClient.OnMessage += HanldeMessage;
+            botClient.StartReceiving();
+
+            
 
             Console.ReadKey();
+            string json = JsonConvert.SerializeObject(users);
+            File.WriteAllText("users.txt", json);
         }
-
-        static async void HanldeMessage(object sender, Telegram.Bot.Args.MessageEventArgs e )
+        
+        static void HanldeMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
             string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
             //Console.WriteLine(e.Message.Text);
             var chatId = e.Message.Chat.Id;
-           await botClient.SendTextMessageAsync(chatId, "Дратути");
+            botClient.SendTextMessageAsync(chatId, "Дратути");
+            var user = new TelegramUser(e.Message.Chat.Id, e.Message.Chat.FirstName);
+            
+            if (!users.Contains(user)) users.Add(user);
+            users[users.IndexOf(user)].addMessage($"{user.Name}:{e.Message.Text}");
+
+           
         }
     }
 
---
+++ HomeWork_09/TelegramUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_09
+{
+    class TelegramUser : IEquatable <TelegramUser>
+    {
+        public TelegramUser (long id,string name)
+        {
+            this.id = id;
+            this.name = name;
+            messages = new List<string>();
+        }
+        private long id;
+
+        public long Id
+        {
+            get { return this.id; }
+            set { id = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public List<string> messages;
+
+        public void addMessage(string msg)
+        {
+            messages.Add(msg);
+        }
+
+        public bool Equals(TelegramUser user) => user.Id == this.id;
+
+
+    }
+}
