0a241ee99aec830919b66293dcd56d5be7bc14d2
Holiday <Holiday@DESKTOP-54GAQ21>
2020-02-21 22:30:54 +0800

    Доделываю принятие картинок

--- HomeWork_09/Command.cs
+++ HomeWork_09/Command.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
@@ -24,7 +23,9 @@ namespace HomeWork_09
                 }
 
 
-                Bot.TelegramBot.OnMessage += HanldeMessage;
+                Bot.TelegramBot.OnMessage += seakMessage;
+                Bot.TelegramBot.OnMessage += seakFile;
+                Bot.TelegramBot.OnMessage += seakPhoto;
                 Bot.TelegramBot.StartReceiving();
             }
             catch (Exception ex)
@@ -38,13 +39,19 @@ namespace HomeWork_09
             
         }
 
-        static void HanldeMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        static void seakMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
+            if(e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            { 
             string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
             Bot.TelegramBot.SendTextMessageAsync(e.Message.Chat.Id, "Дратути");
             Console.WriteLine($"{e.Message.Chat.Username}:{e.Message.Text}");
             var user = new TelegramUser(e.Message.Chat.Id, e.Message.Chat.Username);
-            UsersBase.putUsersMessage(user,e);
+            UsersBase.putUsersMessage(user,e);  
+            }
+        }  
+        static void seakFile(object sender,Telegram.Bot.Args.MessageEventArgs e)
+        {
             if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
             {
                 Console.WriteLine(e.Message.Document.FileId);
@@ -55,27 +62,52 @@ namespace HomeWork_09
                     e.Message.Document.FileName,
                     e.Message.Chat.Username == null ? e.Message.Chat.FirstName : e.Message.Chat.Username,
                     e.Message.Chat.Id);
+                
+                
             }
-            
         }
+
+        static void seakPhoto(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Photo)
+            {
+                Console.WriteLine(e.Message.Photo.Length);
+
+
+                    DownLoad(e.Message.Photo[0].FileId,
+                        "ф"+ e.Message.Photo[0].FileSize +".jpg",
+                        e.Message.Chat.Username == null ? e.Message.Chat.FirstName : e.Message.Chat.Username,
+                        e.Message.Chat.Id);
+                Bot.TelegramBot.SendPhotoAsync(e.Message.Chat.Id, e.Message.Chat.Id + @"\"+"ф" + e.Message.Photo[0].FileSize + ".jpg");
+            }
+        }
+
 
         static async void DownLoad(string field, string path, string name,long id)
         {
-            FileStream fs = null;
             var file = await Bot.TelegramBot.GetFileAsync(field);
-            FileInfo fi = new FileInfo(id + @"\" + path);
-            
-            if (!Directory.Exists(name + @"\" + path))
-            {
-                Directory.CreateDirectory(fi.DirectoryName);
-            }
-               // fs = new FileStream(id + @"\" + path, FileMode.Create);
-            
+            createrFile(id,name,path);
             var user = new TelegramUser(id, name);
             UsersBase.putUsersFile(user, id + @"\" + path);
-            using (BufferedStream bs = new BufferedStream(new FileStream(id + @"\" + path, FileMode.Create)))
-            await Bot.TelegramBot.DownloadFileAsync(file.FilePath, bs);
-            using (BufferedStream bs2 =  new BufferedStream(File.OpenRead(id + @"\" + path))) {
+            try
+            {
+                using (BufferedStream bs = new BufferedStream(new FileStream(id + @"\" + path, FileMode.Create)))
+                await Bot.TelegramBot.DownloadFileAsync(file.FilePath, bs);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                DownLoad(field, path+"1", name, id);
+                return;
+            }
+            Console.WriteLine($"Докачал файл {path}");
+        }
+
+
+        static async void Send(string path,long id)
+        {
+            using (BufferedStream bs2 = new BufferedStream(File.OpenRead(id + @"\" + path)))
+            {
                 InputOnlineFile iof = new InputOnlineFile(bs2, new FileInfo(id + @"\" + path).Name);
                 await Bot.TelegramBot.SendDocumentAsync(
                     chatId: id,
@@ -83,14 +115,16 @@ namespace HomeWork_09
                     caption: "Ваш файл"
                     );
             }
-
         }
 
-
-        static async void Send(string path,long id)
+        static void createrFile(long id ,string name, string path)
         {
-            await Bot.TelegramBot.SendDocumentAsync(id, path);
-        }
+            FileInfo fi = new FileInfo(id + @"\" + path);
 
+            if (!Directory.Exists(name + @"\" + path))
+            {
+                Directory.CreateDirectory(fi.DirectoryName);
+            }
+        }
     }
 }
